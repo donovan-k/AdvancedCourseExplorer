2542e14e7766dfa092a5057d15b3ae67ffa50bce
dkitten2 <dkitten2@illinois.edu>
2022-04-01 19:10:43 -0500

    fixed model problems with code

--- backend/course_exp/admin.py
+++ backend/course_exp/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Course
+from .models import Genedreq
+from .models import Professor
+from .models import Section
+from .models import UserInput
+from .models import UserInfo
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'coursenumber', 'dept', 'description', 'credits')
+
+
+class GenedreqAdmin(admin.ModelAdmin):
+    list_display = ('coursenumber', 'dept', 'yearterm',
+                    'title', 'acp', 'cs', 'hum', 'nat', 'qr', 'sbs')
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'dept')
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('sectionid', 'professorid', 'professors',
+                    'coursenumber', 'dept', 'yearterm', 'avggpa')
+
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password')
+
+
+class UserInputAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'avg_gpa', 'course_req',
+                    'fav_professor', 'interests')
+
+
+# Registering the models
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Genedreq, GenedreqAdmin)
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(UserInfo, UserInfoAdmin)
+admin.site.register(UserInput, UserInputAdmin)
--- backend/course_exp/models.py
+++ backend/course_exp/models.py
@@ -1,62 +1,164 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
 
-# UserInfo model
+class Course(models.Model):
+    course_id = models.IntegerField(db_column='course_id', primary_key=True)
+    coursenumber = models.IntegerField(db_column='CourseNumber')  # Field name made lowercase.
+    dept = models.CharField(db_column='Dept', max_length=30)  # Field name made lowercase.
+    description = models.CharField(db_column='Description', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    credits = models.IntegerField(db_column='Credits', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Course'
+        unique_together = (('coursenumber', 'dept'),)
+
+
+class Genedreq(models.Model):
+    coursenumber = models.IntegerField(db_column='CourseNumber')  # Field name made lowercase.
+    dept = models.CharField(db_column='Dept', max_length=30)  # Field name made lowercase.
+    yearterm = models.CharField(db_column='YearTerm', max_length=255)  # Field name made lowercase.
+    title = models.CharField(db_column='Title', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    acp = models.CharField(db_column='ACP', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    cs = models.CharField(db_column='CS', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    hum = models.CharField(db_column='HUM', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    nat = models.CharField(db_column='NAT', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    qr = models.CharField(db_column='QR', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    sbs = models.CharField(db_column='SBS', max_length=30, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'GenEdReq'
+        unique_together = (('coursenumber', 'dept', 'yearterm'),)
+
+
+class Professor(models.Model):
+    id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=1000)  # Field name made lowercase.
+    dept = models.CharField(db_column='Dept', max_length=30, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Professor'
+        unique_together = (('id', 'name'),)
+
+
+class Section(models.Model):
+    sectionid = models.IntegerField(db_column='SectionID', primary_key=True)  # Field name made lowercase.
+    professorid = models.ForeignKey(Professor, on_delete=models.CASCADE, db_column='ProfessorID', blank=True, null=True)  # Field name made lowercase.
+    professors = models.CharField(db_column='Professors', max_length=1000, blank=True, null=True)  # Field name made lowercase.
+    coursenumber = models.IntegerField(db_column='CourseNumber', blank=True, null=True)  # Field name made lowercase.
+    dept = models.CharField(db_column='Dept', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    yearterm = models.CharField(db_column='YearTerm', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    avggpa = models.CharField(db_column='AvgGPA', max_length=30, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Section'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey(ContentType, models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class UserInfo(models.Model):
-    username = models.CharField(max_length=100, primary_key=True)
+    username = models.CharField(primary_key=True, max_length=100)
     password = models.CharField(max_length=100)
 
+    class Meta:
+        managed = False
+        db_table = 'course_exp_userinfo'
 
-# UserInput model
+
 class UserInput(models.Model):
-    ID = models.PositiveIntegerField(primary_key=True)
+    id = models.PositiveIntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
     username = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    interests = models.CharField(max_length=255, default='0.0.0000986320.8956230.9856230.8456230.22288888')
-    fav_professor = models.CharField(max_length=255, default='')
-    avg_gpa = models.DecimalField(decimal_places=2, max_digits=5, default=-1)
-    course_req = models.CharField(max_length=10, default='')
-
-
-# Course model
-class Course(models.Model):
-    course_number = models.PositiveIntegerField()
-    dept = models.CharField(max_length=10)
-    description = models.CharField(max_length=500)
-    credits = models.PositiveIntegerField()
+    avg_gpa = models.DecimalField(max_digits=5, decimal_places=2)
+    course_req = models.CharField(max_length=10)
+    fav_professor = models.CharField(max_length=255)
+    interests = models.CharField(max_length=255)
 
     class Meta:
-        unique_together = (('course_number', 'dept'),)
+        managed = False
+        db_table = 'course_exp_userinput'
 
 
-# Professor model
-class Professor(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
-    names = models.CharField(max_length=100)
-    dept = models.CharField(max_length=10)
-
-
-# GenEdReq model
-class GenEdReq(models.Model):
-    course_number = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='gen_course_n')
-    dept = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='gen_dept')
-    year_term = models.CharField(max_length=50)
-    title = models.CharField(max_length=255)
-    acp = models.CharField(max_length=30)
-    cs = models.CharField(max_length=30)
-    hum = models.CharField(max_length=30)
-    nat = models.CharField(max_length=30)
-    sbs = models.CharField(max_length=30)
-
-
-# SectionInfo model
-class Section(models.Model):
-    section_id = models.PositiveIntegerField(primary_key=True)
-    professor_id = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    professors = models.CharField(max_length=500)
-    course_number = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='section_course_n')
-    dept = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='section_dept')
-    year_term = models.ForeignKey(GenEdReq, on_delete=models.CASCADE)
-    avg_gpa = models.DecimalField(decimal_places=2, max_digits=5, default=-1)
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey(ContentType, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
 
     class Meta:
-        unique_together = (('course_number', 'dept'),)
+        managed = False
+        db_table = 'django_admin_log'
+        db_table = 'django_sessions'
